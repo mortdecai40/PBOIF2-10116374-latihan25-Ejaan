01b434af71ee41f6770d1c4ebe16987b88f6431e
mortdecai40 <rizkinabil.999@gmail.com>
2020-11-01 10:28:39 +0700

    Latihan 25

---
+++ src/pbo02/pkg10116374/latihan25/perbandingan/PBO0210116374Latihan25Perbandingan.java
@@ -0,0 +1,16 @@
+package pbo02.pkg10116374.latihan25.perbandingan;
+
+/**
+ *
+ * @author Acromyrmex
+ */
+public class PBO0210116374Latihan25Perbandingan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
